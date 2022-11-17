e067313f73643c68987f27bb3d7ccf4c2487c638
Imane <113670994+imanemeradi@users.noreply.github.com>
2022-11-17 11:01:56 +0100

    Update CardValue.java

--- src/main/java/fr/pantheonsorbonne/miage/enums/CardValue.java
+++ src/main/java/fr/pantheonsorbonne/miage/enums/CardValue.java
@@ -3,57 +3,24 @@ package fr.pantheonsorbonne.miage.enums;
 /**
  * An enum that represend the possible cards value from a deck
  */
-public enum CardValue {
-    ACE("1", 14),
-    TWO("2", 2),
-    THREE("3", 3),
-    FOUR("4", 4),
-    FIVE("5", 5),
-    SIX("6", 6),
-    SEVEN("7", 7),
-    EIGHT("8", 8),
-    NINE("9", 9),
-    TEN("10", 10),
-    JACK("J", 11),
-    QUEEN("Q", 12),
-    KING("K", 13);
+public enum CardValue{
+	
+    AS(1), 
+	DEUX(2),
+	TROIS(3),
+	QUATRE(4),
+	CINQ(5),
+	SIX(6),
+	SEPT(7),
+	VALET(8),
+	CAVALIER(9),
+	ROI(10);
 
-    final private String stringRepresentation;
-    final private int rank;
+    final int value;
+	
+	private CardValue(int value) {
+		this.value = value;
+	}
 
-    CardValue(String stringRepresentation, int value) {
-        this.stringRepresentation = stringRepresentation;
-        this.rank = value;
-    }
 
-    /**
-     * From a string representation, return the cad
-     *
-     * @param str
-     * @return the corresponding card
-     * @throws RuntimeException if the representation is invalid
-     */
-    public static CardValue valueOfStr(String str) {
-        for (CardValue value : CardValue.values()) {
-            if (str.equals(value.getStringRepresentation())) {
-                return value;
-            }
-        }
-
-        throw new RuntimeException("failed to parse value");
-
-    }
-
-    public String getStringRepresentation() {
-        return stringRepresentation;
-    }
-
-    /**
-     * the rank of the card for comparison purpose. The higher the rank, the better the card
-     *
-     * @return
-     */
-    public int getRank() {
-        return rank;
-    }
 }
