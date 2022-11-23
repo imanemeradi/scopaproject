59069aa2a6d78046d2148a20987c22c121e322bd
Imane <113670994+imanemeradi@users.noreply.github.com>
2022-11-23 18:25:18 +0100

    ajout de la doc

--- src/main/java/fr/pantheonsorbonne/miage/game/PlayCard.java
+++ src/main/java/fr/pantheonsorbonne/miage/game/PlayCard.java
@@ -7,7 +7,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
+/**
+ * Classe PlayCard qui contient les méthodes et stratégies de jeu
+ * @author Imane Meradi & Sarah Guerrouj
+ * @version 1.0 
+ */
 
 public class PlayCard {
 
@@ -15,12 +19,18 @@ public class PlayCard {
 
 
 
+/** Création d'un objet Card SEPT de DENIERS */
 
 Card septDeDeniers = new Card(CardFigure.DENIERS,CardValue.SEPT);
 
 
 
- 
+/**
+     * cette methode permet de faire une paire avec le 7 de deniers, qu'elle soit dans la main du joueur (premier for)
+     * ou sur la table (deuxieme for), sinon return false
+     * @param player applique le méthode au joueur
+     * @return true si paire avec 7 de deniers sinon false
+     */
 
 public boolean havePairWithSeptDeniers(Player player){
     for( Card card1 : player.getHand()){
@@ -62,6 +72,12 @@ public boolean havePairWithSeptDeniers(Player player){
 }
     
 
+/**
+     * cette methode permet de faire une paire avec une carte de deniers, qu'elle soit dans la main du joueur (premier for)
+     * ou sur la table (deuxieme for), sinon return false
+     * @param player applique le méthode au joueur
+     * @return true si paire avec une carte de deniers sinon false
+     */
 
  
 public boolean havePairWithDeniers(Player player){
@@ -106,9 +122,14 @@ public boolean havePairWithDeniers(Player player){
  }
 
  
- 
- 
- 
+ /**
+     * cette methode permet de faire une paire (simple)
+     * @param player applique le méthode au joueur
+     * @return true si faire une paire simple est possible, false si non, ou false si il ne reste 
+     * que deux cartes sur la table
+     */
+
+
  public boolean haveClassicPair(Player player){
 
  if (( LocalGame.cardOnTheTable.size() ) != 2){ // On ne fait une paire que si il n'ya pas deux cartes sur la table car sinon, on laisserait qu'une seule carte ce qui permettrait au joueur suivant de faire une scopa en cas de pair
@@ -125,8 +146,6 @@ public boolean havePairWithDeniers(Player player){
                 //player.getHand().remove(card1);
                 //LocalGame.cardOnTheTable.remove(card2);
                 return true;
-
-
 
             }
         }
@@ -153,6 +172,14 @@ return false; // Si il n'ya que deux cartes sur la table
 
    
 
+/**
+     * cette methode permet de créée une paire.
+     * Soit avec le 7 de deniers si il est présent sur la table ou dans la main sinon,
+     * une paire avec une carte de deniers quelconque si elle est dans la main ou sur la table sinon, 
+     * une paire quelconque
+     * @param player applique le méthode au joueur
+     * @return true
+     */
 
  
 public boolean getPair(Player player) {
@@ -173,13 +200,20 @@ public boolean getPair(Player player) {
     }
     
 
+    /**
+     * cette methode permet de poser une carte sur la table, dans un premier temps, ne pas poser de cartes
+     * de deniers par strategie, sinon, si il a que des cartes de deniers, posé une carte, mais pas le 7 de deniers
+     * si il est en sa possession
+     * @param player applique le méthode au joueur
+     */
+
     
     
     public void putACardOnTheTable(Player player){
         
         for(Card card : player.getHand()){ 
             if (card.getFigure()!=CardFigure.DENIERS){  // on pose une carte qui n'est pas de deniers par stratégie
-                System.out.println(player.getName() + " a posé la carte " + card + " sur la table");
+                System.out.println(player.getName() + " a posé la carte " + card + " sur la table ");
                 
                
                 LocalGame.cardOnTheTable.add(card);
@@ -209,7 +243,6 @@ public boolean getPair(Player player) {
 }
         
         
-
 
 
 
